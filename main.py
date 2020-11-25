74e44eb0133d1f24ed33086082c7fbdb56716fe6
ZixinLi711 <74847775+ZixinLi711@users.noreply.github.com>
2020-11-25 23:13:48 +0800

    Add files via upload
    improve the speed of indexing and save the storage of indexing. 
    solve the collapse situation.

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 @app.route('/search/<keyword>')
 def searchByKeyword(keyword):
-    num,result = search(keyword)
-    return render_template("page.html",title=search,result=result,keyword=keyword,num=num)
-
+     num,result = search(keyword)
+     return render_template("page.html",title=search,result=result,keyword=keyword,num=num)
 
 from whoosh.index import create_in
 from whoosh.fields import *
 import os.path
 import os
+from whoosh.analysis import StemmingAnalyzer
 def search(keyword):
-     schema = Schema(id = ID(stored=True), submitter = TEXT(stored=True),
-                     authors = TEXT(stored=True), title = TEXT(stored=True),
-                     comments = TEXT(stored=True), journal_ref=TEXT(stored=True),
-                     doi=ID(stored=True),report_no=TEXT(stored=True),
-                     categories= TEXT(stored=True),license= TEXT(stored=True),
-                     abstract= TEXT(stored=True),versions=TEXT(stored=True),
-                     update_date=TEXT(stored=True), authors_parsed= TEXT(stored=True))
+     stem_ana = StemmingAnalyzer(cachesize=-1)
+     schema = Schema(id = ID(stored=True), title = TEXT(stored=True,analyzer=stem_ana),abstract= TEXT(stored=True,analyzer=stem_ana))
 
      if not os.path.exists("index"):
          os.mkdir("index")
      ix = create_in("index", schema)
-     writer = ix.writer()
+     writer = ix.writer(procs=4, multisegment=True, limitmb=2048)
+
+
      import json
      with open('arxiv-metadata-oai-snapshot.json') as f:
          for line in f.readlines():
@@ -35,16 +33,19 @@ def search(keyword):
      from whoosh.qparser import QueryParser,MultifieldParser
      with ix.searcher() as searcher:
          q = MultifieldParser({"title","abstract"},ix.schema).parse(keyword)
-         results = searcher.search(q,limit=None)
+         results = searcher.search(q,limit=None )
+         # results = searcher.collector(collapse="deep learning", collapse_limit = 3)
          list = []
          print(len(results))
+         print(time.process_time())
          for r in results:
-             list.append(dict(r))
+              if not list.__contains__(r):
+                list.append(dict(r))
          return len(list),list
 
 
 
 if __name__ == '__main__':
-     app.run()
+     app.run(port=8081)
 
 
